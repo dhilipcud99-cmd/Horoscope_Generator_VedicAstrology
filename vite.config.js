db4f5c928d9f4617e0870eb2cf8c2d60b979ee30
Dhileeban <pdhileeban09@gmail.com>
2026-07-29 12:15:33 +0530

    chore: configure local build to /docs directory for direct github pages branch deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/Horoscope_Generator_VedicAstrology/' : '/',
-});
+  base: './',
+  build: {
+    outDir: 'docs'
+  }
+});
+
